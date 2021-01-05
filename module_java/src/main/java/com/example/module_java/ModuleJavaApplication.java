5bf05cd16523f7f04f2a85612fa2e371ada40966
xue <1712530035@qq.com>
2021-01-05 15:36:37 +0800

    组件化开发 组件单独调试

--- module_java/src/main/java/com/example/module_java/ModuleJavaApplication.java
+++ module_java/src/main/java/com/example/module_java/ModuleJavaApplication.java
@@ -1,9 +1,10 @@
 package com.example.module_java;
 
 import com.blankj.utilcode.util.LogUtils;
+import com.example.base.BaseApplication;
 import com.example.base.IBaseApplication;
 
-public class ModuleJavaApplication implements IBaseApplication {
+public class ModuleJavaApplication extends BaseApplication implements IBaseApplication {
     @Override
     public void initModuleApplication() {
         LogUtils.e("初始化 模块 Java");
